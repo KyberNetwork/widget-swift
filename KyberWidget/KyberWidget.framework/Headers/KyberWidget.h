aab3dd9059821d23a56583ed96e268e68cbf054e
manhlx <manhlx.300695@gmail.com>
2019-01-23 15:48:02 +0700

    Add zip file

---
+++ KyberWidget/KyberWidget.framework/Headers/KyberWidget.h
@@ -0,0 +1,19 @@
+//
+//  KyberWidget.h
+//  KyberWidget
+//
+//  Created by Manh Le on 27/8/18.
+//  Copyright © 2018 kyber.network. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KyberWidget.
+FOUNDATION_EXPORT double KyberWidgetVersionNumber;
+
+//! Project version string for KyberWidget.
+FOUNDATION_EXPORT const unsigned char KyberWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KyberWidget/PublicHeader.h>
+
+
